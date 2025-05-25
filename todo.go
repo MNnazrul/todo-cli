cd9410d59756997433b3ce5d3e7adb216e5fb4ae
MNnazrul <mnnazrulislam196130@gmail.com>
2025-05-25 22:43:52 +0600

    complete-todo-structure

--- todo.go
+++ todo.go
@@ -1,3 +1,21 @@
 package main
 
+import (
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Todo struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`        
+	Task        string             `bson:"task"`                 
+	Description string             `bson:"description"`          
+	Completed   bool               `bson:"completed"`            
+	CreatedAt   time.Time          `bson:"created_at"`           
+	CompletedAt *time.Time         `bson:"completed_at,omitempty"`
+}
+
+func AddTodo(collection *mongo.Collection, task string) error {
+
+}
